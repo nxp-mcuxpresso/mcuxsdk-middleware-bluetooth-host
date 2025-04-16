eb555dfccb3d1d86280b5836da55e6463048201d
Antoine Bodin <antoine.bodin@nxp.com>
2025-04-16 13:30:42 +0200

    Merge pull request #3493 in CONNINT/bluetooth_public from HCIOT-4903-port-w-uart-mcxw23 to develop/main
    By West PR Extension
    
    Squashed commit of the following:
    
    commit f4bbc4f16caedc8cd0317ba7f114495d86b6d866
    Author: Antoine Bodin <antoine.bodin@nxp.com>
    Date:   Tue Apr 15 16:36:20 2025 +0200
    
        [HCIOT-4903][MCXW23] port w_uart on mcxw23
    
        Signed-off-by: Antoine Bodin <antoine.bodin@nxp.com>

--- examples/w_uart/wireless_uart.c
+++ examples/w_uart/wireless_uart.c
@@ -56,13 +56,7 @@
 #include "board_comp.h"
 #include "wireless_uart.h"
 
-#if defined(K32W232H_SERIES) || defined(KW45B41Z82_SERIES) || defined(KW45B41Z83_SERIES) || defined(K32W1480_SERIES) || \
-    defined(KW47B42ZB7_cm33_core0_SERIES) || defined(KW47B42ZB6_cm33_core0_SERIES) || defined(KW47B42ZB3_cm33_core0_SERIES) || \
-    defined(KW47B42ZB2_cm33_core0_SERIES) || defined(KW47B42Z97_cm33_core0_SERIES) || defined(KW47B42Z96_cm33_core0_SERIES) || \
-    defined(KW47B42Z83_cm33_core0_SERIES) || defined(MCXW716C_SERIES) || defined(MCXW716A_SERIES) || defined(MCXW727C_cm33_core0_SERIES) || \
-    defined(KW43B43ZC7_SERIES)
 #include "sensors.h"
-#endif
 
 #if defined(gUseControllerNotificationsCallback_c) && (gUseControllerNotificationsCallback_c)
     #error "This feature is not available on this board"
