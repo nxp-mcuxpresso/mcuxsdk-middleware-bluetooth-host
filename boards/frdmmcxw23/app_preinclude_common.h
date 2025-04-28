8c8c0696fafb4881b1bdf95dc020bacaead27d6f
Adrian-Constantin Dudau <adrian-constantin.dudau@nxp.com>
2025-04-28 15:58:05 +0200

    Merge pull request #3500 in CONNINT/bluetooth_public from bugfix/BLUETOOTH-15464-sdk_25.06-pvw2-rc1-frdmmcxw236-wireless-uart-advanced-security-variant to develop/main
    By West PR Extension
    
    Squashed commit of the following:
    
    commit 5792deaa5639d919baecb2d5b4da3700d97c3bfa
    Author: Adrian Dudau <adrian-constantin.dudau@nxp.com>
    Date:   Mon Apr 28 13:45:39 2025 +0300
    
        [BLUETOOTH-15464] PACKAGE: Guard defines to prevent user to utilize secure mode feature on unsupported platforms.

--- boards/frdmmcxw23/app_preinclude_common.h
+++ boards/frdmmcxw23/app_preinclude_common.h
@@ -103,6 +103,14 @@
 #define gAppSecureMode_d                 (0U)
 #endif
 
+#if defined(gAppSecureMode_d) && (gAppSecureMode_d == 1U)
+    #error "This platform does not support gAppSecureMode_d"
+#endif /* (gAppSecureMode_d) && (gAppSecureMode_d == 1U) */
+
+#if defined(gSecLibUseSecureSubSystem_d) && (gSecLibUseSecureSubSystem_d == 1U)
+    #error "This platform does not support gSecLibUseSecureSubSystem_d"
+#endif /* (gSecLibUseSecureSubSystem_d) && (gSecLibUseSecureSubSystem_d == 1U) */
+
 #if (gAppSecureMode_d == 1U)
 #define gSecLibSssUseEncryptedKeys_d     (1U)
 #define gHostSecureMode_d                (1U)
