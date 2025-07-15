3c7f2a36e6b8fd173d871f20662955ecbe3743ab
Yassine El Aissaoui <yassine.elaissaoui@nxp.com>
2025-07-15 10:07:08 +0200

    Pull request #3590: [MCSE-915][mcxw23] Allow usage of OTA external flash
    Merge in CONNINT/bluetooth_public from develop/MCSE-915_v2 to develop/main
    
    Squashed commit of the following:
    
    commit 05befe81e1d736ac7c3eae216a82846e76c6cba7
    Author: Yassine El Aissaoui <yassine.elaissaoui@nxp.com>
    Date:   Tue Jul 8 11:25:03 2025 +0200
    
        [MCSE-915][mcxw23] Allow usage of OTA external flash
        
        External OTA support is now provided in framework.
        Allow using it.

--- boards/frdmmcxw23/bluetooth/otac_att/app_preinclude.h
+++ boards/frdmmcxw23/bluetooth/otac_att/app_preinclude.h
@@ -3,7 +3,7 @@
  * @{
  ********************************************************************************** */
 /*
- * Copyright 2021 - 2024 NXP
+ * Copyright 2021 - 2025 NXP
  *
  *
  *
@@ -54,10 +54,6 @@
 
 /*! Define as 1 to place OTA storage in external flash */
 #define gAppOtaExternalStorage_c        (0U)
-
-#if defined(gAppOtaExternalStorage_c) && (gAppOtaExternalStorage_c == 1U)
-    #error "This platform does not support gAppOtaExternalStorage_c"
-#endif /* (gAppOtaExternalStorage_c) && (gAppOtaExternalStorage_c == 1U) */
 
 /*! Define to 1 to post OTA transactions to a queue. The queue will be processed
  * in the idle task. This avoids blocking the system for too long in critical tasks
