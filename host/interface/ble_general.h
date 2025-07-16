9450ebe2cf00afdd0457808ad00c500f55f4fca7
Silviu Petria <silviu.petria@nxp.com>
2025-07-16 15:40:23 +0200

    Pull request #3596: [BLUETOOTH-15832] HOST-GAP: Set gMaxAdvSets_c to 4
    Merge in CONNINT/bluetooth_public from bugfix/BLUETOOTH-15832-kw45-kw47-4-adv-sets-not-enabled-in-bluetooth-host-libraries to develop/main
    
    Squashed commit of the following:
    
    commit 08f4c951f61a7945dcf4e1205cbe7295d03d6e1e
    Author: Silviu Petria <silviu.petria@nxp.com>
    Date:   Wed Jul 16 12:21:20 2025 +0300
    
        [BLUETOOTH-15832] HOST-GAP: Set gMaxAdvSets_c to 4
        
        Increase size of internal Host tables to support 4 advertising sets.
        
        Signed-off-by: Silviu Petria <silviu.petria@nxp.com>

--- host/interface/ble_general.h
+++ host/interface/ble_general.h
@@ -33,7 +33,7 @@
 *************************************************************************************
 ************************************************************************************/
 /* Maximum number of Advertising sets handled by the Host tables. */
-#define gMaxAdvSets_c                           2U
+#define gMaxAdvSets_c                               4U
 
 #define gInvalidDeviceId_c                          (0xFFU)
 #define gInvalidNvmIndex_c                          (0xFFU)
