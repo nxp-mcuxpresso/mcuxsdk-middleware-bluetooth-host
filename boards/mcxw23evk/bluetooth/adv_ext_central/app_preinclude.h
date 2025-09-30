131e4e8bb2a1c5385285f2aff69b8c12eaf1d677
Yassine El Aissaoui <yassine.elaissaoui@nxp.com>
2025-09-30 11:47:12 +0200

    Pull request #3649: Add adv_ext_central & adv_ext_peripheral app support
    Merge in CONNINT/bluetooth_public from develop/MCSE-895/MCSE-896 to develop/main
    
    Squashed commit of the following:
    
    commit 22b266c65a23ae853df60980b7986b5b9b506309
    Author: Yassine El Aissaoui <yassine.elaissaoui@nxp.com>
    Date:   Thu Sep 11 11:10:32 2025 +0200
    
        [MCSE-896][mcxw23][wireless_examples] Add adv_ext_peripheral app support
    
    commit 2c2aad59648c139693c08570c624fd4b28ba2117
    Author: Yassine El Aissaoui <yassine.elaissaoui@nxp.com>
    Date:   Thu Sep 11 11:09:34 2025 +0200
    
        [MCSE-895][mcxw23][wireless_examples] Add adv_ext_central app support

---
+++ boards/mcxw23evk/bluetooth/adv_ext_central/app_preinclude.h
@@ -0,0 +1,156 @@
+/*! *********************************************************************************
+ * \defgroup app
+ * @{
+ ********************************************************************************** */
+/*
+ * Copyright 2021 - 2025 NXP
+ *
+ *
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#ifndef _APP_PREINCLUDE_H_
+#define _APP_PREINCLUDE_H_
+
+/*!
+ *  Application specific configuration file only
+ *  Board Specific Configuration shall be added to board.h file directly such as :
+ *  - Number of buttons on the board,
+ *  - Number of LEDs,
+ *  - etc...
+ */
+/*! *********************************************************************************
+ *     Board Configuration
+ ********************************************************************************** */
+
+/* Number of Buttons required by the application */
+#define gAppButtonCnt_c                             1
+
+/* Number of LEDs required by the application */
+#define gAppLedCnt_c                                1
+
+/*! Enable Debug Console (PRINTF) */
+#define gDebugConsoleEnable_d                       0
+
+/*! *********************************************************************************
+ *     App Configuration
+ ********************************************************************************** */
+/* Enable 5.0 optional features */
+#define gBLE50_d                                    1
+
+/*! Enable/disable printing debug information*/
+#define mAE_CentralDebug_c                          0
+
+/*! Enable/disable use of bonding capability */
+#define gAppUseBonding_d                            1
+
+/*! Enable/disable use of pairing procedure */
+#define gAppUsePairing_d                            1
+
+/*! Enable/disable use of privacy */
+#define gAppUsePrivacy_d                            0
+
+/* Use a Random Static address as identity address, instead of a Public address */
+#define gRandomStaticAddress_d                      1
+
+#define gPasskeyValue_c                             999999
+
+/*! Set the Tx power in dBm */
+#define mAdvertisingDefaultTxPower_c                0
+/*! specifies whether the phy update procedure is going to be initiated in connection or not */
+#define gConnInitiatePhyUpdateRequest_c             (0U)
+
+/*! Repeated Attempts - Mitigation for pairing attacks */
+#define gRepeatedAttempts_d                         0
+/*! *********************************************************************************
+ *     Framework Configuration
+ ********************************************************************************** */
+/* enable NVM to be used as non volatile storage management by the host stack */
+#define gAppUseNvm_d                     1
+
+/* erase NVM pages at download( armgcc only) */
+#if ((defined gAppUseNvm_d) && (gAppUseNvm_d != 0)) && defined(__GNUC__)
+#define gNvmErasePartitionWhenFlashing_c 1U
+#endif
+
+/*! The minimum heap size needed */
+#define MinimalHeapSize_c               14000
+
+/*! Enable/Disable PowerDown functionality in Application - In case of troubles with lowpower,
+      turn it off for debug purpose to ensure your application is fine without lowpower  */
+#define gAppLowpowerEnabled_d           0
+
+/* Disable LEDs when enabling low power */
+#if (defined(gAppLowpowerEnabled_d) && (gAppLowpowerEnabled_d>0))
+  #undef gAppLedCnt_c
+  #define gAppLedCnt_c                0
+#endif
+
+/*! *********************************************************************************
+ *     RTOS Configuration
+ ********************************************************************************** */
+#if defined(SDK_OS_FREE_RTOS)
+/* The following stack sizes have been chosen based on a worst case scenario. 
+ * For different compilers and optimization levels they can be reduced. */
+
+#define gHost_TaskStackSize_c           1800
+
+#define BUTTON_TASK_STACK_SIZE          364
+
+#define SERIAL_MANAGER_TASK_STACK_SIZE  272
+
+#define gMainThreadStackSize_c          1200
+
+#define TM_TASK_STACK_SIZE              384
+
+/* The size used for the Idle task, in dwords. */
+#define configMINIMAL_STACK_SIZE        158
+
+#endif
+
+/*! *********************************************************************************
+ *     BLE Stack Configuration
+ ********************************************************************************** */
+#define gGapSimultaneousEAChainedReports_c          2
+
+#define gMaxServicesCount_d                         6
+#define gMaxServiceCharCount_d                      6
+
+
+/* Enable Serial Manager interface */
+#define gAppUseSerialManager_c                      1
+
+/* See documentation on how to enable DBAF */
+#define gBLE60_DecisionBasedAdvertisingFilteringSupport_d FALSE
+
+#define gAppPAWRSupport_d                                 FALSE
+#define gAppEADSupport_d                                  FALSE
+
+/*! *********************************************************************************
+ *   Xcvr Configuration
+ ********************************************************************************** */
+
+/* Define the max tx power setting in dBm. Allowed values 0, 7 or 10 */
+#if !defined(gAppMaxTxPowerDbm_c)
+#define gAppMaxTxPowerDbm_c 0
+#endif /* !defined(gAppMaxTxPowerDbm_c) */
+
+/*! Set maximum transmit power. */
+#define PLATFORM_MAX_TX_POWER_DBM 10U
+
+/*! *********************************************************************************
+ *  Auto Configuration
+ ********************************************************************************** */
+
+/*! *********************************************************************************
+ *     BLE LL Configuration
+ ***********************************************************************************/
+/* Include common configuration file and board configuration file */
+#include "app_preinclude_common.h"
+#endif /* _APP_PREINCLUDE_H_ */
+
+/*! *********************************************************************************
+ * @}
+ ********************************************************************************** */
