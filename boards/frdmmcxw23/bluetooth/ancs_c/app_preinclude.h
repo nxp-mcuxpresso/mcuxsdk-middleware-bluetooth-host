bae9d60a9284083481fd03ee6a808e4366d3f25e
Yassine El Aissaoui <yassine.elaissaoui@nxp.com>
2025-09-30 12:01:40 +0200

    Pull request #3650: [MCSE-897][mcxw23][wireless_examples] Add ancs_c app support
    Merge in CONNINT/bluetooth_public from develop/MCSE-897 to develop/main
    
    Squashed commit of the following:
    
    commit e1a1144b2b79418b7ada5d2c333b65761265f2d2
    Author: Yassine El Aissaoui <yassine.elaissaoui@nxp.com>
    Date:   Thu Sep 11 16:21:31 2025 +0200
    
        [MCSE-897][mcxw23][wireless_examples] Add ancs_c app support

---
+++ boards/frdmmcxw23/bluetooth/ancs_c/app_preinclude.h
@@ -0,0 +1,127 @@
+/*! *********************************************************************************
+ * \defgroup app
+ * @{
+ ********************************************************************************** */
+/*
+ * Copyright 2025 NXP
+ *
+ *
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#ifndef _APP_PREINCLUDE_H_
+#define _APP_PREINCLUDE_H_
+
+/*!
+ *  Application specific configuration file only
+ *  Board Specific Configuration shall be added to board.h file directly such as :
+ *  - Number of buttons on the board,
+ *  - Number of LEDs,
+ *  - etc...
+ */
+/*! *********************************************************************************
+ *     Board Configuration
+ ********************************************************************************** */
+/* Number of Buttons required by the application */
+#define gAppButtonCnt_c                 1
+
+/* Number of LEDs required by the application */
+#define gAppLedCnt_c                    1
+
+/*! Enable Debug Console (PRINTF) */
+#define gDebugConsoleEnable_d           0
+
+/*! *********************************************************************************
+ *     App Configuration
+ ********************************************************************************** */
+/*! Enable/disable use of bonding capability */
+#define gAppUseBonding_d                1
+
+/*! Enable/disable use of pairing procedure */
+#define gAppUsePairing_d                1
+
+/*! Enable/disable use of privacy */
+#define gAppUsePrivacy_d                0
+
+/* Use a Random Static address as identity address, instead of a Public address */
+#define gRandomStaticAddress_d          1
+
+#define gPasskeyValue_c                 999999
+
+/*! Maximum Number of Services that can be stored during Service Discovery */
+#define gMaxServicesCount_d             10U
+
+/*! Maximum Number of Characteristics per Service that can be stored during Service Discovery */
+#define gMaxServiceCharCount_d          10U
+
+/*! Maximum Number of Descriptors per Service that can be stored during Service Discovery */
+#define gMaxCharDescriptorsCount_d      5U
+
+/*! Repeated Attempts - Mitigation for pairing attacks */
+#define gRepeatedAttempts_d             0
+
+/*! *********************************************************************************
+ *     Framework Configuration
+ ********************************************************************************** */
+/* enable NVM to be used as non volatile storage management by the host stack */
+#define gAppUseNvm_d                     1
+
+/* erase NVM pages at download( armgcc only) */
+#if ((defined gAppUseNvm_d) && (gAppUseNvm_d != 0)) && defined(__GNUC__)
+#define gNvmErasePartitionWhenFlashing_c 1U
+#endif
+
+/* Defines Rx Buffer Size for Serial Manager */
+#define gSerialMgrRxBufSize_c           32
+
+/* Defines Tx Queue Size for Serial Manager */
+#define gSerialMgrTxQueueSize_c         5
+
+/* Defines Size for Serial Manager Task*/
+#define gSerialTaskStackSize_c          700
+
+/*! The minimum heap size needed */
+#define MinimalHeapSize_c               17000
+
+/*! *********************************************************************************
+ *     RTOS Configuration
+ ********************************************************************************** */
+#if defined(SDK_OS_FREE_RTOS)
+/* The following stack sizes have been chosen based on a worst case scenario. 
+ * For different compilers and optimization levels they can be reduced. */
+
+#define gHost_TaskStackSize_c           1920
+
+#endif
+
+/*! *********************************************************************************
+ *     BLE Stack Configuration
+ ********************************************************************************** */
+
+/* Enable Serial Manager interface */
+#define gAppUseSerialManager_c          1
+
+/*! *********************************************************************************
+ *   Xcvr Configuration
+ ********************************************************************************** */
+
+/* Define the max tx power setting in dBm. Allowed values 0, 7 or 10 */
+#if !defined(gAppMaxTxPowerDbm_c)
+#define gAppMaxTxPowerDbm_c 0
+#endif /* !defined(gAppMaxTxPowerDbm_c) */
+
+/*! Set maximum transmit power. */
+#define PLATFORM_MAX_TX_POWER_DBM 10U
+
+/*! *********************************************************************************
+ *     BLE LL Configuration
+ ***********************************************************************************/
+/* Include common configuration file and board configuration file */
+#include "app_preinclude_common.h"
+#endif /* _APP_PREINCLUDE_H_ */
+
+/*! *********************************************************************************
+ * @}
+ ********************************************************************************** */
