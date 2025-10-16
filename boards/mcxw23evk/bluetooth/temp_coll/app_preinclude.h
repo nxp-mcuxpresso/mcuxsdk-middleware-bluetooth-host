9c9404aa56008edf7382c33d3c407f29da4ddfb2
Yassine El Aissaoui <yassine.elaissaoui@nxp.com>
2025-10-16 09:57:49 +0200

    Pull request #3679: Develop/MCSE-901/MCSE-902
    Merge in CONNINT/bluetooth_public from develop/MCSE-901/MCSE-902 to develop/main
    
    Squashed commit of the following:
    
    commit 8577c40f0a368cc998a7a873ca4bf33d90d365ca
    Author: Yassine El Aissaoui <yassine.elaissaoui@nxp.com>
    Date:   Wed Oct 15 12:14:54 2025 +0200
    
        [MCSE-902] APP: Add temp_sens app to W23 platform
    
    commit 91dc698cba2cb00d52fa0662c5fa909d77b78231
    Author: Yassine El Aissaoui <yassine.elaissaoui@nxp.com>
    Date:   Wed Oct 15 12:14:21 2025 +0200
    
        [MCSE-901] APP: Add temp_coll app to W23 platform

---
+++ boards/mcxw23evk/bluetooth/temp_coll/app_preinclude.h
@@ -0,0 +1,121 @@
+/*! *********************************************************************************
+ * \defgroup app
+ * @{
+ ********************************************************************************** */
+/*!
+ * Copyright 2019-2025 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef _APP_PREINCLUDE_H_
+#define _APP_PREINCLUDE_H_
+
+/*!
+ *  Application specific configuration file only
+ *  Board Specific Configuration shall be added to board.h file directly such as :
+ *  - Number of button on the board,
+ *  - Number of LEDs,
+ *  - etc...
+ */
+/*! *********************************************************************************
+ *     Board Configuration
+ ********************************************************************************** */
+/* Number of Button required by the application */
+#define gAppButtonCnt_c                 1
+
+/* Number of LED required by the application */
+#define gAppLedCnt_c                    1
+
+/*! Enable Debug Console (PRINTF) */
+#define gDebugConsoleEnable_d           0
+
+/* Enable Serial Manager interface */
+#define gAppUseSerialManager_c          1
+
+/*! *********************************************************************************
+ *     App Configuration
+ ********************************************************************************** */
+/*! Enable/disable use of bonding capability */
+#define gAppUseBonding_d   1
+
+/*! Enable/disable use of pairing procedure */
+#define gAppUsePairing_d   1
+
+/*! Enable/disable use of privacy */
+#define gAppUsePrivacy_d   0
+
+/* Use a Random Static address as identity address, instead of a Public address */
+#define gRandomStaticAddress_d 1
+
+#define gPasskeyValue_c                999999
+
+#define gMaxServicesCount_d            6
+
+/*! specifies whether the phy update procedure is going to be initiated in connection or not */
+#define gConnInitiatePhyUpdateRequest_c            (1U)
+
+/*! Repeated Attempts - Mitigation for pairing attacks */
+#define gRepeatedAttempts_d             0
+/*! *********************************************************************************
+ *     Framework Configuration
+ ********************************************************************************** */
+/* enable NVM to be used as non volatile storage management by the host stack */
+#define gAppUseNvm_d                     1
+
+/* erase NVM pages at download( armgcc only) */
+#if ((defined gAppUseNvm_d) && (gAppUseNvm_d != 0)) && defined(__GNUC__)
+#define gNvmErasePartitionWhenFlashing_c 1U
+#endif
+
+/*! The minimum heap size needed */
+#define MinimalHeapSize_c               12000
+
+/* Disable LEDs when enabling low power */
+#if (defined(gAppLowpowerEnabled_d) && (gAppLowpowerEnabled_d>0))
+  #undef gAppLedCnt_c
+  #define gAppLedCnt_c                0
+#endif
+
+/*! Enable the SWD pins to be managed into low-power */
+#define gBoard_ManageSwdPinsInLowPower_d    0
+
+/*! *********************************************************************************
+ *     RTOS Configuration
+ ********************************************************************************** */
+#if defined(SDK_OS_FREE_RTOS)
+
+#define BUTTON_TASK_STACK_SIZE          380
+
+#define SERIAL_MANAGER_TASK_STACK_SIZE  320
+
+#define TM_TASK_STACK_SIZE              360
+
+/* The size used for the Idle task, in dwords. */
+#define configMINIMAL_STACK_SIZE        156
+
+#endif /* defined(SDK_OS_FREE_RTOS) */
+
+/*! *********************************************************************************
+ *   Xcvr Configuration
+ ********************************************************************************** */
+
+/* Define the max tx power setting in dBm. Allowed values 0, 2 or 6 */
+#if !defined(gAppMaxTxPowerDbm_c)
+#define gAppMaxTxPowerDbm_c 0
+#endif /* !defined(gAppMaxTxPowerDbm_c) */
+
+#if gAppMaxTxPowerDbm_c > 6
+#error "gAppMaxTxPowerDbm_c 6dBm is the maximum supported"
+#endif
+
+/*! *********************************************************************************
+ *     BLE LL Configuration
+ ***********************************************************************************/
+/* Include common configuration file and board configuration file */
+#include "app_preinclude_common.h"
+#endif /* _APP_PREINCLUDE_H_ */
+
+/*! *********************************************************************************
+ * @}
+ ********************************************************************************** */
