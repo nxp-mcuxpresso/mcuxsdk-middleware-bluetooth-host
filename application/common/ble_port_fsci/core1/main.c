1dbb180c6fdeed957cecf6717d2a38f3cc0b8488
Axel Le Bourhis <axel.lebourhis@nxp.com>
2025-10-22 10:44:52 +0200

    Pull request #3683: Revert "Pull request #3667: [MCSE-1134] Set BT_ECLK back to 32MHz"
    Merge in CONNINT/bluetooth_public from develop/MCSE-1198 to develop/main
    
    Squashed commit of the following:
    
    commit e216208697819997f21a285471cbec1d9399700b
    Author: Axel Le Bourhis <axel.lebourhis@nxp.com>
    Date:   Tue Oct 21 12:50:16 2025 +0200
    
        Revert "Pull request #3667: [MCSE-1134] Set BT_ECLK back to 32MHz"
        
        This reverts commit 152237ee195b5a37cf5d49b29bb14822598315d1.

--- application/common/ble_port_fsci/core1/main.c
+++ application/common/ble_port_fsci/core1/main.c
@@ -348,6 +348,13 @@ int main(void)
     /* Not enabling the Systicks now, will be done in _tx_thread_schedule */
     SysTick->CTRL  = SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_TICKINT_Msk;
 
+#if !defined(RF_OSC_26MHZ) || (RF_OSC_26MHZ == 0)
+    /* bt_eclk should be >=16MHz, so for XO=26MHz select XO while for XO=32MHz XO/2 can be selected
+     * This allows to save some power during active mode */
+    RADIO_CTRL->RF_CLK_CTRL &= ~RADIO_CTRL_RF_CLK_CTRL_BT_ECLK_DIV_MASK;
+    RADIO_CTRL->RF_CLK_CTRL |= RADIO_CTRL_RF_CLK_CTRL_BT_ECLK_DIV(0x1U);
+#endif
+
     /* Init OSA: should be called before any other OSA API*/
     OSA_Init();
     Controller_RadioInit();
@@ -391,8 +398,8 @@ int __main(void)
 ********************************************************************************** */
 void SystemInitHook (void)
 {
-    /* Configure NBU memory mapping as early as possible in the SystemInitHook()
-     * to prevent any potential issues
+    /* Configure NBU memory mapping as early as possible in the SystemInitHook() 
+     * to prevent any potential issues 
      */
     PLATFORM_ConfigureSmuDmemMapping();
 }
