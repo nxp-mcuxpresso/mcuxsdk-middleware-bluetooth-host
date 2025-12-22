7fbe8f25a86d2615092009f7bc9bac52794924ca
Andrei Istodorescu <andrei.istodorescu@nxp.com>
2025-12-22 16:23:16 +0100

    Pull request #3745: Revert "Pull request #3744: [BLUETOOTH-16701] APP: Apply 0.5m compensation for all boards"
    Merge in CONNINT/bluetooth_public from bugfix/BLUETOOTH-16706-sdk_25.12-repo-nightly-553-kw47evk-all-loc-apps-failed-due-to-distance-cde to develop/main
    
    Squashed commit of the following:
    
    commit e76a7e559ba2972b8977617a8bab8120c4b9569d
    Author: Andrei Istodorescu <andrei.istodorescu@nxp.com>
    Date:   Mon Dec 22 15:38:46 2025 +0200
    
        Revert "Pull request #3744: [BLUETOOTH-16701] APP: Apply 0.5m compensation for all boards"
        
        This reverts commit 3fc9617b42fb530e76690efe190add5f43258b6a.

--- application/common/lcl/app_localization.c
+++ application/common/lcl/app_localization.c
@@ -187,12 +187,16 @@ static uint16_t maPsmChannels[gAppMaxConnections_c];
 static TIMER_MANAGER_HANDLE_DEFINE(mTemperatureTimerId);
 
 /* PCT Rotation calibration parameters */
-/* 50cm compensation for EVK and LOC boards */
-static uint8_t maPctRotationParams[4U * APP_LOCALIZATION_MAX_NO_ANTENNAS] = {57U, 1U, 0U, 0U,
-                                                                             57U, 1U, 0U, 0U,
-                                                                             57U, 1U, 0U, 0U,
-                                                                             57U, 1U, 0U, 0U};
-
+#if defined (BOARD_LOCALIZATION_REVISION_SUPPORT) && (BOARD_LOCALIZATION_REVISION_SUPPORT == 1U)
+/* 0cm compensation for LOC boards */
+static uint8_t maPctRotationParams[4U * APP_LOCALIZATION_MAX_NO_ANTENNAS] = {0U};
+#else
+/* 15cm compensation for EVK boards */
+static uint8_t maPctRotationParams[4U * APP_LOCALIZATION_MAX_NO_ANTENNAS] = {67U, 0U, 0U, 0U,
+                                                                             67U, 0U, 0U, 0U,
+                                                                             67U, 0U, 0U, 0U,
+                                                                             67U, 0U, 0U, 0U};
+#endif
 /************************************************************************************
 *************************************************************************************
 * Private prototypes
