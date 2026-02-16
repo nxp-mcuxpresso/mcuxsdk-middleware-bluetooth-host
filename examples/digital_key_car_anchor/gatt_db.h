9b9bf1cb839bc3c0ae4386f9ffe741f879cb698f
Silviu Petria <silviu.petria@nxp.com>
2026-02-16 13:38:22 +0100

    Pull request #3810: [BLUETOOTH-17038] APP: Fix PSM VDBT Version characteristic definition
    Merge in CONNINT/bluetooth_public from bugfix/BLUETOOTH-17038-iphone17-vs-kw47-channel-sounding-iop to develop/main
    
    Squashed commit of the following:
    
    commit c220136deee37bc8690fd6d9c3340980052d8f3a
    Author: Silviu Petria <silviu.petria@nxp.com>
    Date:   Mon Feb 16 10:51:11 2026 +0200
    
        [BLUETOOTH-17038] APP: Fix PSM VDBT Version characteristic definition
        
        Length byte refers to the number of versions supported, not to the
        length in bytes of the versions supported field.
        
        Signed-off-by: Silviu Petria <silviu.petria@nxp.com>

--- examples/digital_key_car_anchor/gatt_db.h
+++ examples/digital_key_car_anchor/gatt_db.h
@@ -13,7 +13,7 @@ PRIMARY_SERVICE(service_dk, gBleSig_CCC_DK_UUID_d)
     CHARACTERISTIC_UUID128(char_vehicle_psm, uuid_char_vehicle_psm, (gGattCharPropRead_c) )
             VALUE_UUID128(value_vehicle_psm, uuid_char_vehicle_psm, (gPermissionFlagReadable_c), 2, MSB2(gDK_DefaultVehiclePsm_c), LSB2(gDK_DefaultVehiclePsm_c))
     CHARACTERISTIC_UUID128(char_vehicle_psm_vdbt_version, uuid_char_vehicle_psm_vdbt_version, (gGattCharPropRead_c) )
-            VALUE_UUID128(value_vehicle_psm_vdbt_version, uuid_char_vehicle_psm_vdbt_version, (gPermissionFlagReadable_c | gPermissionFlagReadWithEncryption_c | gPermissionFlagReadWithAuthentication_c), 7, MSB2(gDK_DefaultVehiclePsm_c), LSB2(gDK_DefaultVehiclePsm_c), 2, 0x03, 0x00, 1, 0xFF)
+            VALUE_UUID128(value_vehicle_psm_vdbt_version, uuid_char_vehicle_psm_vdbt_version, (gPermissionFlagReadable_c | gPermissionFlagReadWithEncryption_c | gPermissionFlagReadWithAuthentication_c), 7, MSB2(gDK_DefaultVehiclePsm_c), LSB2(gDK_DefaultVehiclePsm_c), 1, 0x03, 0x00, 1, 0xFF)
     CHARACTERISTIC_UUID128(char_device_selected_vdbt_version, uuid_char_device_selected_vdbt_version, (gGattCharPropWrite_c) )
             VALUE_UUID128(value_device_selected_vdbt_version, uuid_char_device_selected_vdbt_version, (gPermissionFlagWritable_c | gPermissionFlagWriteWithEncryption_c | gPermissionFlagWriteWithAuthentication_c), 4, 0x00, 0x00, 0x00, 0x00)
     CHARACTERISTIC(char_tx_power_level, gBleSig_TxPower_d, (gGattCharPropRead_c) )
@@ -25,7 +25,7 @@ PRIMARY_SERVICE(service_dk, gBleSig_CCC_DK_UUID_d)
 /*! *********************************************************************************
 * \file gatt_db.h
 *
-* Copyright 2021-2025 NXP
+* Copyright 2021-2026 NXP
 *
 * SPDX-License-Identifier: BSD-3-Clause
 ********************************************************************************** */
--- examples/digital_key_car_anchor_cs/gatt_db.h
+++ examples/digital_key_car_anchor_cs/gatt_db.h
@@ -13,13 +13,13 @@ PRIMARY_SERVICE(service_dk, gBleSig_CCC_DK_UUID_d)
     CHARACTERISTIC_UUID128(char_vehicle_psm, uuid_char_vehicle_psm, (gGattCharPropRead_c) )
             VALUE_UUID128(value_vehicle_psm, uuid_char_vehicle_psm, (gPermissionFlagReadable_c), 2, MSB2(gDK_DefaultVehiclePsm_c), LSB2(gDK_DefaultVehiclePsm_c))
 CHARACTERISTIC_UUID128(char_vehicle_psm_vdbt_version, uuid_char_vehicle_psm_vdbt_version, (gGattCharPropRead_c) )
-            VALUE_UUID128(value_vehicle_psm_vdbt_version, uuid_char_vehicle_psm_vdbt_version, (gPermissionFlagReadable_c | gPermissionFlagReadWithEncryption_c | gPermissionFlagReadWithAuthentication_c), 7, MSB2(gDK_DefaultVehiclePsm_c), LSB2(gDK_DefaultVehiclePsm_c), 2, 0x03, 0x00, 1, 0xFF)
+            VALUE_UUID128(value_vehicle_psm_vdbt_version, uuid_char_vehicle_psm_vdbt_version, (gPermissionFlagReadable_c | gPermissionFlagReadWithEncryption_c | gPermissionFlagReadWithAuthentication_c), 7, MSB2(gDK_DefaultVehiclePsm_c), LSB2(gDK_DefaultVehiclePsm_c), 1, 0x03, 0x00, 1, 0xFF)
 
 /* Placed at the end of file due to macro interpretation of line numbers. */
 /*! *********************************************************************************
 * \file gatt_db.h
 *
-* Copyright 2022 - 2025 NXP
+* Copyright 2022 - 2026 NXP
 *
 * SPDX-License-Identifier: BSD-3-Clause
 ********************************************************************************** */
