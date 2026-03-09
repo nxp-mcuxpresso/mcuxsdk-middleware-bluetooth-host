6cdceb107c88ca73371aebfddfcd495ae07873f4
Silviu Petria <silviu.petria@nxp.com>
2026-03-09 10:33:16 +0100

    Pull request #3859: [MCUX-86573] APP: License update for localization files
    Merge in CONNINT/bluetooth_public from bugfix/MCUX-86573-license-clr-sdk-next-repo-bluetooth_public-license-info-in-file-header-needs-to-be to develop/main
    
    Squashed commit of the following:
    
    commit 052968ff3e6b939de1c9c97cad9c7cd894feb621
    Author: Silviu Petria <silviu.petria@nxp.com>
    Date:   Mon Mar 9 10:15:22 2026 +0200
    
        [MCUX-86573] APP: License update for localization files
        
        Signed-off-by: Silviu Petria <silviu.petria@nxp.com>

--- examples/digital_key_car_anchor_cs/gatt_db.h
+++ examples/digital_key_car_anchor_cs/gatt_db.h
@@ -21,9 +21,12 @@ CHARACTERISTIC_UUID128(char_vehicle_psm_vdbt_version, uuid_char_vehicle_psm_vdbt
 *
 * Copyright 2022 - 2026 NXP
 *
-* NXP Confidential Proprietary
+* NXP Proprietary
 *
-* No part of this document must be reproduced in any form - including copied,
-* transcribed, printed or by any electronic means - without specific written
-* permission from NXP.
+* This software is owned or controlled by NXP and may only be used strictly in
+* accordance with the applicable license terms. By expressly accepting such terms or
+* by downloading, installing, activating and/or otherwise using the software, you are
+* agreeing that you have read, and that you agree to comply with and are bound by,
+* such license terms. If you do not agree to be bound by the applicable license terms,
+* then you may not retain, install, activate or otherwise use the software.
 ********************************************************************************** */
