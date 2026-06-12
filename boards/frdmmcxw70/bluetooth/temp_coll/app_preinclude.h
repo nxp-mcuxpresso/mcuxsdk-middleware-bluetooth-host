c877170896a79cf2bc64519ab670b8e846e44b52
Dragos Adrian Musoiu <dragos.musoiu@nxp.com>
2026-06-12 10:07:30 +0200

    Pull request #4081: [BLUETOOTH-17849]APPLICATIONS: Increase host task stack size for temp_coll and temp_sensor
    Merge in CONNINT/bluetooth_public from bugfix/BLUETOOTH-17849-2606-rc1-mw30-temperature_collector_freertos-and-temperature_sensor_bm-app to develop/main
    
    Squashed commit of the following:
    
    commit 14659a3f3d7ea2069f11fd1e73b169400d84881d
    Author: WBI\nxa14931 <dragos.musoiu@nxp.com>
    Date:   Thu Jun 11 16:40:34 2026 +0300
    
        [BLUETOOTH-17849]APPLICATIONS: Increase host task stack size for temp_coll and temp_sensor

--- boards/frdmmcxw70/bluetooth/temp_coll/app_preinclude.h
+++ boards/frdmmcxw70/bluetooth/temp_coll/app_preinclude.h
@@ -88,7 +88,7 @@
 /* The following stack sizes have been chosen based on a worst case scenario. 
  * For different compilers and optimization levels they can be reduced. */
 
-#define gHost_TaskStackSize_c           1764
+#define gHost_TaskStackSize_c           2200
 
 #define BUTTON_TASK_STACK_SIZE          380
 
